b0afe49223eb4e35c4006093670e653b0222f2ee
sidrakhtar <reachsidraakhtar@gmail.com>
2022-08-08 11:32:05 -0700

    define URL and view function

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+class Finch: 
+   def __init__(self, name, family, habitat, diet, price, lifespan):
+      self.name = name
+      self.family = family
+      self.habitat = habitat
+      self.diet = diet
+      self.price = price
+      self.lifespan = lifespan
+
+finches = [
+   Finch('Blue Finch', 'Thraupidae', 'Dry Savannah', 'Seeds, insects and plant matter', '$20-$40', '5-10 years'),
+   Finch('Purple Finch', 'suburbs, groves, and woods', 'Dry Savannah', 'Seeds(of elm, and ash and so on), berries, small fruits, buds,  insects', '$55-$150', '5-9 years on average'),
+]
+
 
 # Create your views here.
+# Define the home view
+def home(request):
+   return HttpResponse('<h1>Home Page</h1>')
+
+def about(request):
+   return render(request, 'about.html')
+
+def finches_index(request):
+   return render(request, 'finches/index.html', { 
+      'finches': finches 
+      })
