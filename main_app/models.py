b67b9a01e06c900e45600d11e6620188f2175661
sidrakhtar <reachsidraakhtar@gmail.com>
2022-08-12 00:31:14 -0700

    Add Toy model and all of its CRUD functionality

--- main_app/models.py
+++ main_app/models.py
@@ -1,14 +1,26 @@
-from os import DirEntry
 from django.db import models
 # Import the reverse function
 from django.urls import reverse
+from datetime import date
 
-# Create your models here.
 MEALS = (
    ('B', 'Breakfast'),
    ('L', 'Lunch'),
    ('D', 'Dinner'),
 )
+
+# Create your models here.
+
+class Toy(models.Model):
+   name = models.CharField(max_length=50)
+   color = models.CharField(max_length=20)
+
+   def __str__(self):
+      return self.name
+
+   def get_absolute_url(self):
+      return reverse('toys_detail', kwargs={'pk': self.id})
+
 
 class Finch(models.Model):
    name = models.CharField(max_length=100)
@@ -21,6 +33,13 @@ class Finch(models.Model):
    def __str__(self):
       return f'{self.name} ({self.id})'
 
+   def get_absolute_url(self):
+      return reverse('detail', kwargs={'finch_id': self.id})
+
+   def fed_for_today(self):
+      return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+
+
 class Feeding(models.Model):
    date = models.DateField('Feeding Date')
    meal = models.CharField(
@@ -28,7 +47,10 @@ class Feeding(models.Model):
       choices=MEALS,
       default=MEALS[0][0]
       )
-   finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+   finch = models.ForeignKey(
+      Finch, 
+      on_delete=models.CASCADE
+   )
 
    def __str__(self):
       # Nice method for obtaining the friendly value of a Field.choice
@@ -37,5 +59,4 @@ class Feeding(models.Model):
    class Meta:
       ordering = ['-date']
 
-   def get_absolute_url(self):
-      return reverse('detail', kwargs={'finch_id': self.id})
+   
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Finch
+from django.views.generic import ListView, DetailView
+from .models import Finch, Toy
 from .forms import FeedingForm
 
 # Create your views here.
@@ -21,10 +22,27 @@ def finches_detail(request, finch_id):
    finch = Finch.objects.get(id=finch_id)
    # instantiate FeedingForm to be rendered in the template
    feeding_form = FeedingForm()
-   return render(request, 'finches/detail.html', {
-      'finch': finch,
-      'feeding_form': feeding_form
-      })
+   return render(
+      request, 
+      'finches/detail.html',
+      {
+         'finch': finch,
+         'feeding_form': feeding_form
+      }
+   )
+
+class FinchCreate(CreateView):
+   model = Finch
+   fields = '__all__'
+   # success_url = '/finches/' --- used "get_absolute_url" method in models.py
+
+class FinchUpdate(UpdateView):
+   model = Finch
+   fields = ['family', 'habitat', 'diet', 'price', 'lifespan']
+
+class FinchDelete(DeleteView):
+   model = Finch
+   success_url = '/finches/'
 
 def add_feeding(request, finch_id):
    # create a ModelForm instance using the data in request.POST
@@ -39,15 +57,20 @@ def add_feeding(request, finch_id):
    return redirect('detail', finch_id=finch_id)
 
 
-class FinchCreate(CreateView):
-   model = Finch
-   fields = '__all__'
-   # success_url = '/finches/' --- used "get_absolute_url" method in models.py
+class ToyList(ListView):
+  model = Toy
 
-class FinchUpdate(UpdateView):
-   model = Finch
-   fields = ['family', 'habitat', 'diet', 'price', 'lifespan']
+class ToyDetail(DetailView):
+  model = Toy
 
-class FinchDelete(DeleteView):
-   model = Finch
-   success_url = '/finches/'
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
