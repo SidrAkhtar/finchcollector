e2ea13aa0dcf8b1c0f6978619be19237e3025351
sidrakhtar <reachsidraakhtar@gmail.com>
2022-08-12 18:35:45 -0700

    add upload photo functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,10 @@
+import os
+import uuid
+import boto3
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
-from .models import Finch, Toy
+from .models import Finch, Toy, Photo
 from .forms import FeedingForm
 
 # Create your views here.
@@ -58,7 +61,7 @@ def add_feeding(request, finch_id):
    # validate the form
    if form.is_valid():
       # don't save the form to the db until it
-      # has the cat_id assigned
+      # has the finch_id assigned
       new_feeding = form.save(commit=False)
       new_feeding.finch_id = finch_id
       new_feeding.save()
@@ -92,3 +95,23 @@ class ToyUpdate(UpdateView):
 class ToyDelete(DeleteView):
   model = Toy
   success_url = '/toys/'
+
+def add_photo(request, finch_id):
+   # photo-file will be the "name" attribute on the <input type="file">
+   photo_file = request.FILES.get('photo-file', None)
+   if photo_file:
+      s3 = boto3.client('s3')
+      # need a unique "key" for S3 / needs image file extension too
+      key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+      # just in case something goes wrong
+      try:
+         bucket = os.environ['S3_BUCKET']
+         s3.upload_fileobj(photo_file, bucket, key)
+         # build the full url string
+         url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
+         # we can assign to finch_id or finch (if you have a finch object)
+         Photo.objects.create(url=url, finch_id=finch_id)
+      except Exception as e:
+         print('An error occurred uploading file to S3')
+         print(e)
+   return redirect('detail', finch_id=finch_id)
